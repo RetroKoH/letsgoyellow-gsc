1a875a80d05bf543bdc246998964e1da8b6fc339
SonicRetroKingofHarts <cmgaudet2010@gmail.com>
2021-02-19 22:20:19 +0700

    Reverted Pallet Town to RBY
    Layout, Objects, Music

--- maps/PalletTown.asm
+++ maps/PalletTown.asm
@@ -17,62 +17,49 @@ PalletTown_MapScriptHeader:
 	bg_event 13, 13, SIGNPOST_JUMPTEXT, OaksLabSignText
 	bg_event 11,  5, SIGNPOST_JUMPTEXT, BluesHouseSignText
 
-	db 3 ; object events
+	db 2 ; object events
 	object_event  3,  8, SPRITE_AROMA_LADY, SPRITEMOVEDATA_WANDER, 2, 2, -1, -1, 0, PERSONTYPE_COMMAND, jumptextfaceplayer, UnknownText_0x1ac6e7, -1
-	object_event 12, 14, SPRITE_FAT_GUY, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 0, 2, -1, -1, PAL_NPC_GREEN, PERSONTYPE_COMMAND, jumptextfaceplayer, UnknownText_0x1ac720, -1
-	object_event 17,  7, SPRITE_SCHOOLBOY, SPRITEMOVEDATA_WALK_UP_DOWN, 2, 0, -1, -1, 0, PERSONTYPE_COMMAND, jumptextfaceplayer, PalletTownYoungsterText, -1
+	object_event 11, 14, SPRITE_FAT_GUY, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 0, 2, -1, -1, PAL_NPC_GREEN, PERSONTYPE_COMMAND, jumptextfaceplayer, UnknownText_0x1ac720, -1
 
 PalletTownFlyPoint:
 	setflag ENGINE_FLYPOINT_PALLET
 	return
 
 UnknownText_0x1ac6e7:
-	text "I'm raising #-"
-	line "mon too."
+	text "I'm raising"
+	line "#MON too!"
 
-	para "They serve as my"
-	line "private guards."
+	para "When they get"
+	line "strong, they can"
+	cont "protect me!"
 	done
 
 UnknownText_0x1ac720:
 	text "Technology is"
 	line "incredible!"
 
-	para "You can now make"
-	line "games just by"
-
-	para "writing code in-"
-	line "stead of editing"
-	cont "binary data."
-	done
-
-PalletTownYoungsterText:
-	text "Smell ya later!"
-
-	para "…People started"
-	line "saying that around"
-	cont "here, but it's"
-
-	para "kinda weird if you"
-	line "think about it."
+	para "You can now store"
+	line "and recall items"
+	cont "and #MON as"
+	cont "data via PC!"
 	done
 
 PalletTownSignText:
-	text "Pallet Town"
+	text "PALLET TOWN"
 
-	para "A Tranquil Setting"
-	line "of Peace & Purity"
+	para "Shades of your"
+	line "journey await!"
 	done
 
 RedsHouseSignText:
-	text "Red's House"
+	text "<PLAYER>'s HOUSE"
 	done
 
 OaksLabSignText:
-	text "Oak #mon"
-	line "Research Lab"
+	text "OAK #MON"
+	line "RESEARCH LAB"
 	done
 
 BluesHouseSignText:
-	text "Blue's House"
+	text "<RIVAL>'s HOUSE"
 	done

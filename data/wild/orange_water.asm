5d42d61329f510e8863c143f35908136a5622993
RetroKoH-Base <cmgaudet2010@gmail.com>
2023-05-06 02:09:37 +0700

    Removed all unused wild encounter data

--- data/wild/orange_water.asm
+++ data/wild/orange_water.asm
@@ -1,10 +1,2 @@
 ; Orange Pokémon in water
-
-	def_water_wildmons NAVEL_ROCK_OUTSIDE
-	db 6 percent ; encounter rate
-	wildmon 80, TENTACRUEL
-	wildmon 80, STARMIE
-	wildmon 80, LANTURN
-	end_water_wildmons
-
 	db -1 ; end
--- data/wild/swarm_water.asm
+++ data/wild/swarm_water.asm
@@ -1,5 +1,3 @@
 ; swarms in water
-
 	; No swarms encountered while surfing in Crystal
-
 	db -1 ; end

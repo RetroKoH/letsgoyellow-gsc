376ef9b51f8a89090ee653bf94f4fbe220f5d411
KoH <cmgaudet2010@gmail.com>
2024-01-06 14:21:22 +0700

    Minor palette change to Option Menu

--- engine/menus/init_options.asm
+++ engine/menus/init_options.asm
@@ -128,8 +128,8 @@ SetInitialOptions:
 .BGPalette:
 if !DEF(MONOCHROME)
 	RGB 31, 31, 31
-	RGB 09, 30, 31
-	RGB 01, 11, 31
+	RGB 29, 26, 05
+	RGB 17, 10, 08
 	RGB 00, 00, 00
 else
 	MONOCHROME_RGB_FOUR
